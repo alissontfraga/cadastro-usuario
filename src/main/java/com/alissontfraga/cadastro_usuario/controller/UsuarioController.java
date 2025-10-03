8939c16cb6573e45eb8ead04c2ca1fed0a74f35b
Alisson Teles Fraga <alissontfraga@gmail.com>
2025-10-03 12:49:00 -0300

    feat: implement user controller

---
+++ src/main/java/com/alissontfraga/cadastro_usuario/controller/UsuarioController.java
@@ -0,0 +1,25 @@
+package com.alissontfraga.cadastro_usuario.controller;
+
+import com.alissontfraga.cadastro_usuario.business.UsuarioService;
+import com.alissontfraga.cadastro_usuario.infrastructure.entities.Usuario;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController                     // Diz que essa classe é um controller REST (API)
+@RequestMapping("/usuario")         // Define a rota base -> tudo aqui começa com /usuario
+@RequiredArgsConstructor            // Gera um construtor automático para os atributos "final"
+
+public class UsuarioController {
+
+    private final UsuarioService usuarioService; // Injeta o serviço que lida com regras de negócio
+
+    @PostMapping                                      // Diz que esse métod responde a requisições HTTP POST
+    public ResponseEntity<Void> salvarUsuario(@RequestBody Usuario usuario){
+        usuarioService.salvarUsuario(usuario);                      // chama o service para salvar o usuário
+        return ResponseEntity.ok().build();                         // retorna resposta HTTP 200 (sucesso)
+    }
+}
